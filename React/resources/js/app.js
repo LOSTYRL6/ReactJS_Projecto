bf7b7ef82e9ae070e4921cba09e0d97e87f271c6
Pablo Lin <162104715+LOSTYRL6@users.noreply.github.com>
2025-05-06 15:18:45 +0200

    Inicio mas o menos

--- React/resources/js/app.js
+++ React/resources/js/app.js
@@ -1,2 +1,3 @@
 import './bootstrap';
 import './Componente/MiPrimerComponente'
+import './Componente/Navbar'
--- React/routes/web.php
+++ React/routes/web.php
@@ -14,5 +14,5 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('Layouts.LayoutsPrincipal');
+    return view('home');
 });
